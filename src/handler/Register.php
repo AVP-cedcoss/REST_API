4ccbc81c71defeb83e87fb4c8d30caea598d274c
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-04-20 18:17:10 +0530

    task completed

---
+++ src/handler/Register.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Handler;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+use Phalcon\Di\Injectable;
+
+class Register extends Injectable
+{
+    private $mongo = ' ';
+
+    public function __construct()
+    {
+        $this->mongo = (new \MongoDB\Client('mongodb://mongo', array('username' => 'root', "password" => 'password123')))->mongodb;
+    }
+
+    /**
+     * Register Form Display
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $html='
+        <form method="POST" action="/register/signup">
+            <input type="text" name="user_name" placeholder="username" required>
+            <input type="email" name="user_email" placeholder="email" required>
+            <input type="password" name="user_password" placeholder="password" required>
+            <input type="submit">
+        </form>
+        ';
+        return $html;
+    }
+
+    /**
+     * Signup Handle
+     *
+     * @return void
+     */
+    public function signup()
+    {
+        $user = array(
+            'user_email' => $this->request->getPost('user_email'),
+            'user_name' => $this->request->getPost('user_name'),
+            'user_password' => $this->request->getPost('user_password'),
+            'user_role' => 'User',
+        );
+
+        $this->mongo->user->insertOne($user);
+        header('location: /register/login');
+    }
+    
+    /**
+     * Login Form Display
+     *
+     * @return void
+     */
+    public function login()
+    {
+        $html='
+        <form method="POST" action="/register/signin">
+            <input type="email" name="user_email" placeholder="email" required>
+            <input type="password" name="user_password" placeholder="password" required>
+            <input type="submit">
+        </form>
+        ';
+        return $html;
+    }
+
+    /**
+     * Login Handle
+     *
+     * @return void
+     */
+    public function signin()
+    {
+        $user = array(
+            'user_email' => $this->request->getPost('user_email'),
+            'user_password' => $this->request->getPost('user_password')
+        );
+        $result = $this->mongo->user->findOne(
+            [
+                "user_email" => $this->request->getPost('user_email'),
+                'user_password' => $this->request->getPost('user_password')
+            ]
+        );
+
+        if (isset($result->_id)) {
+            header('location: /register/getToken');
+        } else {
+            header('location: /register/login');
+        }
+    }
+
+    public function getToken()
+    {
+        $html='
+        <form method="GET" action="/register/generateToken">
+            <input type="submit" value="Generate Token">
+        </form>
+        ';
+        return $html;
+    }
+
+    public function generateToken()
+    {
+        $key = "anugrah_vishwas_paul";
+        
+        $now        = new \DateTime();
+        $issued     = $now->getTimestamp();
+        $notBefore  = $now->modify('+5 minute')->getTimestamp();
+        
+        $payload = array(
+            "iat" => $issued,
+            "exp" => $notBefore,
+            "sub" => 'granted',
+        );
+
+        $jwt = JWT::encode($payload, $key, 'HS256');
+        return $jwt;
+    }
+
+    public function expired()
+    {
+        $html = '
+        <form method="GET" action="/register/generateToken">
+            Token Expired. Kindly Generate a New One.
+            <input type="submit" value="Click Me!">
+        </form>
+        ';
+        return $html;
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,159 @@
+<?php
+
+error_reporting(E_ALL);
+
+use Phalcon\Loader;
+use Phalcon\Mvc\Micro;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Router;
+
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', __DIR__);
+// define('APP_PATH', BASE_PATH . '/app');
+define('URL_ROOT', 'http://localhost:8080');
+require_once(BASE_PATH."/vendor/autoload.php");
+
+$container = new FactoryDefault();
+
+$loader = new Loader();
+
+$loader->registerNamespaces(
+    [
+        'Handler' => BASE_PATH . '/handler',
+    ]
+);
+
+$loader->register();
+
+$container->set(
+    'mongo',
+    function () {
+        $mongo =  new \MongoDB\Client('mongodb://mongo', array('username'=>'root',"password"=>'password123'));
+        return $mongo->mongodb;
+    }
+);
+
+
+
+$product = new Handler\Product();
+$register = new Handler\Register();
+
+
+$app = new Micro($container);
+
+/**------------------------------------------------------GET REQUEST START------------------------------------------------- */
+
+/* -----------------------REGISTER Start------------------*/
+$app->get(
+    '/register',
+    [
+        $register,
+        'register'
+    ]
+);
+
+$app->post(
+    '/register/signup',
+    [
+        $register,
+        'signup'
+    ]
+);
+
+$app->get(
+    '/register/login',
+    [
+        $register,
+        'login'
+    ]
+);
+
+$app->post(
+    '/register/signin',
+    [
+        $register,
+        'signin'
+    ]
+);
+
+$app->get(
+    '/register/getToken',
+    [
+        $register,
+        'getToken'
+    ]
+);
+
+$app->get(
+    '/register/generateToken',
+    [
+        $register,
+        'generateToken'
+    ]
+);
+
+$app->get(
+    '/register/expired',
+    [
+        $register,
+        'expired'
+    ]
+);
+
+/* -----------------------REGISTER End------------------*/
+
+
+$app->get(
+    '/',
+    [
+        $product,
+        'help'
+    ]
+);
+
+$app->before(
+    function () use ($app) {
+        if (str_contains($_SERVER['REQUEST_URI'], 'products')) {
+            if (null === ($app->request->getQuery("access_token"))) {
+                echo "Kindly Provide Access Token";
+                die;
+            }
+        }
+    }
+);
+
+$app->get(
+    '/products/get',
+    [
+        $product,
+        'all'
+    ]
+);
+
+$app->get(
+    '/products/get/{limit}/{page}',
+    [
+        $product,
+        'all'
+    ]
+);
+
+$app->get(
+    '/products/search/{keyword}',
+    [
+        $product,
+        'findByKeyword'
+    ]
+);
+
+$app->notFound( 
+    function () use($app) {
+        echo "Invalid URL Kindly Check the ENDPOINT! or ACCESS TOKEN not Provided";
+    }
+);
+
+/**------------------------------------------------------GET REQUEST END------------------------------------------------- */
+
+$app->handle(
+    $_SERVER['REQUEST_URI']
+);
